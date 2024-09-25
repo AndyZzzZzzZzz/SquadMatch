246e23740220c39f9de8b21c94c3f1b01689e86d
Tongyue Zhao <tza95@sfu.ca>
2024-09-24 22:46:03 -0700

    db_dev_fix_lint_issue

--- db/migrate/20240925023925_create_events.rb
+++ db/migrate/20240925023925_create_events.rb
@@ -1,14 +1,14 @@
 class CreateEvents < ActiveRecord::Migration[7.2]
   def change
     create_table :events do |t|
-      t.references :host, null: false, foreign_key: { to_table: :users }  
-      t.references :club, null: false, foreign_key: true   
-      t.references :event_type, null: false, foreign_key: true 
-      t.string :title, null: false        
-      t.integer :capacity, null: false       
+      t.references :host, null: false, foreign_key: { to_table: :users }
+      t.references :club, null: false, foreign_key: true
+      t.references :event_type, null: false, foreign_key: true
+      t.string :title, null: false
+      t.integer :capacity, null: false
       t.string :location, null: false
-      t.date :event_date, null:false
-      t.time :event_time, null:false       
+      t.date :event_date, null: false
+      t.time :event_time, null: false
       t.text :description                    
       t.timestamps
     end
