03f04c0e5c07d1a6efad4458440b1294979c16a8
Andy Zhang <az@MacBook-Pro-738.local>
2024-09-29 23:00:04 -0700

    Added comments

--- config/initializers/clear_prepared_statements.rb
+++ config/initializers/clear_prepared_statements.rb
@@ -1,5 +1,8 @@
-# config/initializers/clear_prepared_statements.rb
 
+<!-- 
+  Targets PostgreSQL adapters, ensures all prepared statements are cleared when ActiveRecord (db for rails) loads a new connection from
+  the cpnnection pool. tells PostgreSQL to deallocate all prepared statements for the current session. Ensures a clean development enviornment
+-->
 ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.connection_pool.with_connection do |conn|
       if conn.adapter_name == 'PostgreSQL'
