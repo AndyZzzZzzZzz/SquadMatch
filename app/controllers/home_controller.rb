e3e97193bd6ec5e972ab0094934905b7c01138a0
Tongyue Zhao <tza95@sfu.ca>
2024-10-06 12:25:30 -0700

    update dynamic news_WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     @events = Event.includes(:host, :club, :event_type, :category)
       .where("event_datetime >= ?", DateTime.now)
       .order(event_datetime: :asc) # Order by datetime, default to ascending
+    @latest_news = News.order(created_at :desc).limit(5)
   end
 
   def dashboard
