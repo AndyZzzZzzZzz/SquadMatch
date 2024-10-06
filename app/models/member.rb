259790eb14419c8cc90cb50560eb3e82fc8f0559
Tongyue Zhao <tza95@sfu.ca>
2024-10-06 11:35:22 -0700

    db_dev: members_fix

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,7 @@
 class Member < ApplicationRecord
     self.primary_key = [:user_id, :club_id]
-    belongs_to: user
-    belongs_to: club
+    belongs_to :user
+    belongs_to :club
 
     validates :join_at, presence: true
 end
