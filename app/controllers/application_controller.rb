632a7d3314363dd736207ec4450bcbe650865bf9
AndyZzzZzzZzz <zkpAndy@outlook.com>
2024-10-29 23:22:07 -0700

    Added rails default controller to redirect login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def require_login 
+    unless current_user
+      redirects_to login_path, alert: "You must be logged in to access this section."
+    end
+  end
 end
