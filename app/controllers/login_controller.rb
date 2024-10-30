8ac3e07d530e2340f9916cff24c5f4b777cd17b5
Tongyue Zhao <tza95@sfu.ca>
2024-10-29 19:56:45 -0700

    a simple profile display

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -27,6 +27,6 @@ class LoginController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path, notice: "Logged out successfully"
+    redirect_to home_path, notice: "Logged out successfully"
   end
 end
