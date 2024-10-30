0e1e8ba9eb133542f43b0362df8590fcc3d7dc62
AndyZzzZzzZzz <zkpAndy@outlook.com>
2024-10-29 23:31:32 -0700

    Redirects to login page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
+  # require user login
+  before_action :require_login
+  
   def index
   end
 end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,7 @@
 class ProfileController < ApplicationController
+  # require user login
+  before_action :require_login
+  
   def index
     @user = current_user
   end
